d53dfe00ec03ff09aa2485388498d4feece5c917
ewqc <guaine86@gmail.com>
2025-03-03 16:40:06 -0300

    cookie-asistencia

--- public/js/main.js
+++ public/js/main.js
@@ -26,6 +26,10 @@ const miModulo = (() => {
                 return lanzaSWAL("error", "Oops...", "Complete todos los campos!!");
             }
 
+            if(document.cookie.includes(`clase=${datos.curso}`)){
+                return lanzaSWAL("error", "Oops..", "Ya has registrado tu asistencia para esta clase!!");
+            }
+
             const response = await fetch('/api/registrar', {
                 method: 'POST',
                 headers: {
@@ -37,6 +41,12 @@ const miModulo = (() => {
             const icono = !result.message ? "info" : "success";
             const texto = !result.message ? result.error : result.message;
             // alert(result.message);
+            if(result.message === 'Asistencia registrada con exito!'){
+                const expirationDate = new Date();
+                expirationDate.setTime(expirationDate.getTime() + 2*60*60*1000);
+                document.cookie = `clase=${datos.curso}; expires=${expirationDate.toUTCString()}; path=/`;
+            }
+            
             lanzaSWAL(icono, "Info Solicitud:", texto);
 
             document.querySelector('#curso').disabled = true;
